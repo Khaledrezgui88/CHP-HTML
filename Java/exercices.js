6f44a6daf74f202f69fd46850afc48649fa24ff8
Khaledrezgui88 <khaled.rezgui@esprit.tn>
2024-07-01 17:56:53 +0100

    checkpoint java

---
+++ Java/exercices.js
@@ -0,0 +1,137 @@
+// exercice1   0.75/1
+// var question = prompt("What'is your name ?");
+// alert("bienvenue");
+// var question = confirm("Are you sure to continue");
+
+
+//exercice2 100%
+// let nom= "khaled";
+// let prenom = "Rezgui";
+// console.log("Le nom est : " + nom);
+// console.log("Le prenom est : " + prenom);
+// console.log("Nom Prenom : " + (nom + prenom));
+
+ 
+//exercice 3  done 
+
+
+//exercice 4 60%
+
+// affiche un message de type boolean (true,false)
+// let a=true;
+// let b="k";
+// let c=5;
+// let d
+// let e=null
+// affiche un message de type boolean (true,false)
+//  console.log(typeof(a))
+ // affiche un message de type string
+//  console.log(typeof(b))
+  // affiche un message de type number
+//  console.log(typeof(c))
+//affiche un message de type undefined
+// console.log(typeof(d))
+// affiche un message de type  object
+// console.log(typeof(e))
+
+//exercice5 100%
+// operateur arithmetique 
+// let a=5;
+// let b=3;
+// console.log(a+b); =8
+// console.log(a-b); =2
+// console.log(a/b); =1.66666
+// console.log(a*b); =15
+// console.log(a%b); =2
+// console.log(a**b); =125
+
+// operateur comparaison
+// const b = 4;
+// const a = 2;
+// console.log( a>b ); =false
+// console.log( a<b ) ;=true
+// console.log( a==b );  = false
+/// console.log( a===b );  = false
+// console.log( a!=b );  = true
+// console.log( a<=b ) ; = true
+// console.log( a>=b );  = false
+
+// operateur logique
+// const a = 3;
+// const b = 2;
+// console.log(a > 10 && b > 0); = false
+// console.log(a > 10 || b > 0);  = true
+// console.log(!a); = false
+
+
+//exercice6
+// affiche khaledRezgui
+// var x = "khaled";
+// x += "Rezgui";
+// console.log(x);
+
+// affiche 5
+// var x = 10;
+// x -= 5;
+// console.log(x);
+
+//  affiche 50
+// var x = 10;
+//     x *= 5;
+//  console.log(x);
+
+//  affiche 2
+// var x = 10;
+//     x /= 5;
+//  console.log(x);
+//  affiche 0
+//  var x = 10;
+//     x %= 5;
+//  console.log(x);
+
+
+//exercice7
+// let message = "Bonjour, monde !";
+//affiche le langueur du message
+// console.log(message.length); 
+
+//affiche le message en majuscul
+// console.log(message.toUpperCase());
+
+//affiche le message en minuscule
+// console.log(message.toLowerCase()); 
+
+// indice auquel un élément donné peut être trouvé
+// console.log(message.indexOf("monde")); 
+
+//remplacer le encien mot par un nouveau mot
+//  console.log(message.replace("monde", "tout le monde")); 
+
+
+
+let nom = "azyz";
+let age = 30;
+let competences = ["HTML", "CSS", "JAVASCRIPT", "React JS"];
+console.log(nom, age, competences);
+
+// Remplacer le ancien Nom azyz par nouveau nom khaled
+console.log(nom.replace("azyz", "khaled"));
+//Remplacer le ancien age 30 nouveau age 35
+age = 35;
+console.log(age);
+//Remplacer le ancien competence par le competence
+competences.push("Boostrap", "tailwind", "Java","react JS");
+console.log(competences);
+
+
+
+
+
+
+
+
+
+
+
+
+
